2473360ea20b40ebe1f2569984eca590299e0fb5
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-06-02 15:34:01 +0530

    updated

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'PASSWORD': '',
         # 'PORT': '3306',
             }
         }
