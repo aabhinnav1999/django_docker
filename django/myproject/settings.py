995719d7558fe53ba90db73524be1072333a455c
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-06-07 12:50:36 +0530

    added staticfiles

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -128,8 +128,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS=[BASE_DIR/'static_folder']
+STATIC_ROOT = BASE_DIR/'staticfiles'
+# STATICFILES_DIRS=[BASE_DIR/'static_folder']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
