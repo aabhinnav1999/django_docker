2871b634191f849cd111f15eef9f61c93a229ae8
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-06-07 13:10:23 +0530

    added car image

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
-# STATICFILES_DIRS=[BASE_DIR/'static_folder']
+STATICFILES_DIRS=[BASE_DIR/'static_folder']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
