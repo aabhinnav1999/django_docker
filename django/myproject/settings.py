7dc5e3c24def028fa104e3f2dde3fcec054bde60
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-06-07 12:05:23 +0530

    updated database

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django_db',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'django_mysql_container',
-#         'PORT': '3306',
-#             }
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'django_mysql_container',
+        'PORT': '3306',
+            }
+        }
 
 
 # Password validation
